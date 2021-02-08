4fb1b71c169f8094a393ff158bcb358bf8d04ab5
prakhardubey001 <prakhar.dubey@cedcoss.com>
2021-02-08 10:59:29 +0530

    initial commit

---
+++ app/src/main/java/com/example/pushnotification/MyFirebaseMessagingService.kt
@@ -0,0 +1,84 @@
+package com.example.pushnotification
+
+import com.example.pushnotification.R
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    var TAG:String="my service"
+    override
+    fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.from)
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.size > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+            if ( /* Check if data needs to be processed by long running job */true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+//                scheduleJob()
+            } else {
+                // Handle message within 10 seconds
+               handleNow(remoteMessage)
+            }
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body)
+            handleNow(remoteMessage)
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+    override fun onNewToken(p0: String) {
+        Log.d(TAG, p0)
+    }
+    private fun handleNow(remoteMessage: RemoteMessage) {
+
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0, intent, PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val channelId = getString(R.string.app_name)
+        val defaultSoundUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder: NotificationCompat.Builder
+        notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(remoteMessage.notification!!.title)
+            .setContentText(remoteMessage.notification!!.body)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+// Since android Oreo notification channel is needed.
+
+// Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+}
